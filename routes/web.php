3364ce1723e8c1a414776d27b0a150fe235dbae0
ziaulkamal <ziaul.kamal1109@gmail.com>
2025-02-08 14:47:38 +0700

    dev

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 // Route::get('/login', [AuthController::class, 'showLoginForm']);
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::get('/', function () {
+    return view('welcome');
+});
