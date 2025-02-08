ebfb2c057e2cba1b27ad5c05c6a35940922cfcfb
ziaulkamal <ziaul.kamal1109@gmail.com>
2025-02-08 19:24:12 +0700

    update

--- config/cors.php
+++ config/cors.php
@@ -5,7 +5,8 @@ return [
     'allowed_methods' => ['*'],
     'allowed_origins' => [
         'http://localhost:3000',
-        'http://103.127.96.144:8101'
+        'http://103.127.96.144:8101',
+        'http://103.127.96.144:8121',
     ], // Ganti dengan URL frontend Anda
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
