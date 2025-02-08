6e632e920fcfa7fe32bf32d817686b73fea738f6
ziaulkamal <ziaul.kamal1109@gmail.com>
2025-02-08 18:57:18 +0700

    update

--- config/cors.php
+++ config/cors.php
@@ -3,7 +3,10 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:3000', 'http://103.127.96.144:8101/'], // Ganti dengan URL frontend Anda
+    'allowed_origins' => [
+        'http://localhost:3000',
+        'http://103.127.96.144:8101'
+    ], // Ganti dengan URL frontend Anda
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
