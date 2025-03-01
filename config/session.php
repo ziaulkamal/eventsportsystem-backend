7241ddddffbc449a1bac8b17219646bf0ac25132
ziaulkamal <ziaul.kamal1109@gmail.com>
2025-03-01 13:27:05 +0700

    update Nav Mobile Log out

--- config/session.php
+++ config/session.php
@@ -3,40 +3,24 @@
 use Illuminate\Support\Str;
 
 return [
-
     'driver' => env('SESSION_DRIVER', 'file'),
-
     'lifetime' => env('SESSION_LIFETIME', 120),
-
     'expire_on_close' => false,
-
     'encrypt' => false,
-
     'files' => storage_path('framework/sessions'),
-
     'connection' => env('SESSION_CONNECTION'),
-
     'table' => 'sessions',
-
     'store' => env('SESSION_STORE'),
-
     'lottery' => [2, 100],
-
     'cookie' => env(
         'SESSION_COOKIE',
         Str::slug(env('APP_NAME', 'psix'), '_') . '_session'
     ),
-
     'path' => '/',
-
     'domain' => env('SESSION_DOMAIN', null), // Pastikan domain diset jika multi-domain digunakan
-
     'secure' => env('SESSION_SECURE_COOKIE', true), // Pastikan cookie hanya dikirim melalui HTTPS
-
     'http_only' => true, // Cookie tidak bisa diakses via JavaScript
-
     'same_site' => env('SESSION_SAME_SITE', 'lax'), // Harus 'none' agar dapat digunakan dengan credentials: 'include'
-
     'partitioned' => false, // Tidak perlu mengaktifkan partitioned cookie
 
 ];
--- resources/views/components/head.blade.php
+++ resources/views/components/head.blade.php
@@ -165,10 +165,14 @@
             </div>
         </li>
     </ul>
+
+
     <div class="dropdown mobile-user-menu">
+        <a href="javascript:void(0);" class="nav-link dropdown-toggle" data-bs-toggle="dropdown"
+            aria-expanded="false"><i class="fa fa-ellipsis-v"></i></a>
         <div class="dropdown-menu dropdown-menu-right">
-            <button class="dropdown-item" id="logout-mobile">Logout</button>
+
+            <button class="dropdown-item logout pb-0" id="logout-mobile">Logout</button>
         </div>
     </div>
-    <!-- /Mobile Menu -->
 </div>
